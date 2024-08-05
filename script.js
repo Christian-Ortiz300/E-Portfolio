af2b8d645c0069779042fcdc309b377fa1c69337
Christian-Ortiz300 <Christianortiz039@gmail.com>
2024-08-04 23:22:53 -0500

    completed layout, just working on js issues and making sure that the email part works properly.

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+/*Active Navbar Link*/
+let sections = document.querySelector('section'); 
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll = () => {
+    sections.array.forEach(element => {
+        
+    });
+}; 
+
+
 /*Sticky navbar */
 window.onscroll = () => {
 let header = document.querySelector('.header'); 
 
 header.classList.toggle('sticky', window.scrollY > 100); 
 };
-
 
 /*Dark light mode*/ 
 let darkModeIcon = document.querySelector('#darkMode-icon'); 
