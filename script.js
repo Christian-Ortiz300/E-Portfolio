fbfcd72c5389bad66789873c9bc5ff00a835dab0
Christian-Ortiz300 <Christianortiz039@gmail.com>
2024-08-15 00:29:33 -0500

    fixed the animation within the background

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 /*Active Navbar Link*/
-let sections = document.querySelector('section'); 
-let navLinks = document.querySelector('header nav a');
+let sections = document.querySelectorAll('section'); 
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.array.forEach(element => {
-        
+        let top = window.scrollY; 
+        let offset = sec.offsetTop - 150; 
+        let height = sec.offsetHeight; 
+        let id = sec.getAttribute('id'); 
+
+        if(top >= offset && top < offset + height){ 
+
+        };
     });
 }; 
 
